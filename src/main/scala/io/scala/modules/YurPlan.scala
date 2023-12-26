8fe7e16931b4de034ec8deb47d77e6435a578699
Lucas Nouguier <lucas.nouguier@protonmail.com>
2023-12-26 18:56:36 +0100

    changed yurplan logo + renamed root main.js

--- src/main/scala/io/scala/modules/YurPlan.scala
+++ src/main/scala/io/scala/modules/YurPlan.scala
@@ -7,17 +7,9 @@ object YurPlan:
     div(
       className := "yurplan-container",
       iframe(
-        src := "https://yurplan.com/orga/Scala-IO/12404/ticketing/widget?wversion=1",
-        className := "yurplan-widget",
+        src            := "https://yurplan.com/events/Scala-IO-2024/115152/tickets/widget?wversion=1&culture=fr",
+        className      := "yurplan-widget",
         dataAttr("id") := "12404"
       ),
-      a(
-        href := "https://yurplan.com/billetterie-en-ligne?utm_source=referral&utm_medium=widget&utm_campaign=lien_billetterie_widget",
-        target := "_blank",
-        className := "yurplan-widget-link-12404",
-        "Logiciel de gestion d'événements"
-      )
-      // script(
-      //   src := "https://assets.yurplan.com/yurplan-v1/dist/widget.js",
-      //   `type` := "text/javascript")
+      scriptTag(src := "https://assets.yurplan.com/yurplan-v1/dist/widget.js", `type` := "text/javascript")
     )
