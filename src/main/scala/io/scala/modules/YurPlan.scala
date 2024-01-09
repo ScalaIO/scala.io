079d8bd8bacba6b461c21c50fb6a8ea78e324aea
Lucas Nouguier <lucas.nouguier@protonmail.com>
2024-01-09 15:06:02 +0100

    yurplan iframe title + height fix

--- src/main/scala/io/scala/modules/YurPlan.scala
+++ src/main/scala/io/scala/modules/YurPlan.scala
@@ -7,6 +7,7 @@ object YurPlan:
     div(
       className := "yurplan-container",
       iframe(
+        title          := "Tickets ScalaIO 2024",
         src            := "https://yurplan.com/events/Scala-IO-2024/115152/tickets/widget?wversion=1&culture=fr",
         className      := "yurplan-widget",
         dataAttr("id") := "12404"
