f7dadd86c2ab37d1ec838a5acf8ab6ccacb3ed92
Lucas Nouguier <lucas.nouguier@protonmail.com>
2024-01-16 02:38:50 +0100

    center cards

--- src/main/scala/io/scala/utils/Screen.scala
+++ src/main/scala/io/scala/utils/Screen.scala
@@ -1,9 +1,14 @@
 package io.scala.utils
 
+import io.scala.data.TalksInfo
+import io.scala.views.blankDivs
+
 import com.raquo.airstream.state.Var
+import com.raquo.laminar.api.L.{*, given}
 import org.scalajs.dom
 import org.scalajs.dom.UIEvent
 import org.scalajs.dom.console
+import org.scalajs.dom.document
 import org.scalajs.dom.window
 
 enum Screen:
@@ -16,6 +21,26 @@ object Screen:
   window.onresize = { _ =>
     val newScreen = width
     if newScreen != screenVar.now() then screenVar.set(newScreen)
+  }
+
+  window.onresize = { _ =>
+    console.log("recomputing blank divs...")
+    val width         = window.innerWidth
+    val cardContainer = document.querySelector(".talks-list > .card-container")
+    val card          = document.querySelector(".talks-list > .card-container > .talk-card")
+
+    (cardContainer, card) match
+      case (null, _) | (_, null) => ()
+      case (container, el) =>
+        val containerWidth = container.getBoundingClientRect().width
+        val cardWidth      = el.getBoundingClientRect().width + 32
+        val cardsByLine    = Math.floor(containerWidth / cardWidth).toInt
+        val remainingCards = TalksInfo.allTalks.filter(_.speakers.forall(_.confirmed)).length % cardsByLine
+
+        console.log(s"containerWidth: $containerWidth")
+        console.log(s"cardWidth: $cardWidth")
+
+        blankDivs.set(List.fill(cardsByLine - remainingCards)(div(className := "blank-div")))
   }
 
   def fromWidth(width: Double): Screen = width match
--- src/main/scala/io/scala/views/TalksList.scala
+++ src/main/scala/io/scala/views/TalksList.scala
@@ -1,23 +1,29 @@
 package io.scala.views
 
-import com.raquo.laminar.api.L.{*, given}
 import io.scala.Lexicon
 import io.scala.data.TalksInfo
 import io.scala.domaines.*
 import io.scala.modules.TalkCard
 import io.scala.modules.elements.*
 
+import com.raquo.laminar.api.L.{*, given}
+import org.scalajs.dom.console
+import org.scalajs.dom.document
+import org.scalajs.dom.window
+
+val blankDivs: Var[List[Div]] = Var(List.empty)
+
 case object TalksList extends SimpleView {
+
   def bodyContent(talks: List[Talk]) = sectionTag(
     className := "container talks-list",
     Title("Talks"),
     Line(margin = 55),
     div(
-      div(
-        talks.map(TalkCard(_)),
-        className := "card-container"
-      ),
-    )
+      talks.map(TalkCard(_)),
+      children <-- blankDivs.signal,
+      className := "card-container"
+    ),
   )
 
   def body(withDraft: Boolean): HtmlElement =
