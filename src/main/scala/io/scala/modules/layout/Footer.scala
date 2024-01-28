23444b07e5736cdb40299bd474dff1d14d63e34a
Lucas Nouguier <lucas.nouguier@protonmail.com>
2024-01-28 18:00:11 +0100

    accessibility

--- src/main/scala/io/scala/modules/layout/Footer.scala
+++ src/main/scala/io/scala/modules/layout/Footer.scala
@@ -1,7 +1,5 @@
 package io.scala.modules.layout
 
-import com.raquo.laminar.api.L.{*, given}
-import com.raquo.laminar.nodes.ReactiveHtmlElement
 import io.scala.Lexicon
 import io.scala.modules.elements.Line
 import io.scala.modules.elements.LineKind
@@ -9,6 +7,9 @@ import io.scala.modules.elements.ShinyButton
 import io.scala.svgs.Linkedin
 import io.scala.svgs.Logo
 import io.scala.svgs.Twitter
+
+import com.raquo.laminar.api.L.{*, given}
+import com.raquo.laminar.nodes.ReactiveHtmlElement
 import org.scalajs.dom.html
 
 object Footer {
@@ -25,32 +26,32 @@ object Footer {
         Lexicon.Footer.Newsletter.title,
         className := "title-small"
       ),
-      div(
-        div(
-          Lexicon.Footer.Newsletter.description,
-          className := "description"
+      p(
+        Lexicon.Footer.Newsletter.description,
+        className := "description"
+      ),
+      form(
+        action := "https://scala.us21.list-manage.com/subscribe/post?u=1cda6358b5f519bda81ecb37a&amp;id=e31dddaa5a&amp;f_id=005af1e6f0",
+        method     := "post",
+        idAttr     := "mc-embedded-subscribe-form",
+        target     := "_blank",
+        noValidate := true,
+        input(
+          idAttr     := "mce-EMAIL",
+          typ        := "email",
+          nameAttr   := "EMAIL",
+          aria.label := "Search"
         ),
-        form(
-          action := "https://scala.us21.list-manage.com/subscribe/post?u=1cda6358b5f519bda81ecb37a&amp;id=e31dddaa5a&amp;f_id=005af1e6f0",
-          method := "post",
-          idAttr := "mc-embedded-subscribe-form",
-          target := "_blank",
-          noValidate := true,
-          input(
-            idAttr    := "mce-EMAIL",
-            typ       := "email",
-            nameAttr  := "EMAIL",
-          ),
-          ShinyButton(Lexicon.Footer.Newsletter.button),
-          className := "form"
-        ),
+        ShinyButton(Lexicon.Footer.Newsletter.button),
+        className := "form"
       ),
       className := "newsletter"
     )
 
-  def social(icon: SvgElement, url: String) = a(
+  def social(icon: SvgElement, url: String, name: String) = a(
     icon,
-    href      := url,
+    href := url,
+    nameAttr := name
   )
 
   lazy val render: HtmlElement = footerTag(
@@ -67,8 +68,8 @@ object Footer {
         className := "copyright"
       ),
       div(
-        social(Linkedin(), "https://www.linkedin.com/company/scala-io"),
-        social(Twitter(), "https://twitter.com/ScalaIO_FR"),
+        social(Linkedin(), "https://www.linkedin.com/company/scala-io", "Linkedin ScalaIO"),
+        social(Twitter(), "https://twitter.com/ScalaIO_FR", "Twitter ScalaIO"),
         className := "social"
       ),
       className := "others"
