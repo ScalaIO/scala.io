8705b26b4387806b1f8a6b71ea484fca532180d4
Lucas Nouguier <lucas.nouguier@protonmail.com>
2024-02-12 15:41:53 +0100

    Added Samuel Bernard

--- src/main/scala/io/scala/data/SpeakersInfo.scala
+++ src/main/scala/io/scala/data/SpeakersInfo.scala
@@ -379,4 +379,18 @@ object SpeakersInfo:
     confirmed = true
   )
 
+  val samuelBernard = Speaker(
+    name = "Samuel Bernard",
+    photo = Some("sBernard.webp"),
+    description = "After a PhD in Distributed Systems, Samuel left the academic world to be a software engineer in a fast growing scale-up. There he learned how to build cutting-edge softwares and data infrastructures. Over the years he refined his skills and became an expert in DevOps/DataOps and at managing tech teams. Samuel has been an early employee of Criteo, the Technology Innovation leader at S4M, co-founder & CTO at Make.org, and an independent coach/advisor of start-ups & data teams.",
+    job = "Co-Founder & CTO",
+    company = "NuMind",
+    socials = List(
+      Social(Social.Kind.Twitter, "https://twitter.com/TaXules"),
+      Social(Social.Kind.Linkedin, "https://www.linkedin.com/in/samuelbernardphd"),
+      Social(Social.Kind.Gitlab, "https://gitlab.com/samuel.bernard")
+    ),
+    confirmed = true
+  )
+
   val allSpeakers = allTalks.flatMap(_.speakers).distinct.sortBy(_.name)
