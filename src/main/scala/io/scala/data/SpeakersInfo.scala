71937cd9138bce2685816b8b9ac7fb35ec60538e
Jim Newton <jnewton@lrde.epita.fr>
2024-02-14 13:43:58 +0100

    added bio (Jim Newton)

--- src/main/scala/io/scala/data/SpeakersInfo.scala
+++ src/main/scala/io/scala/data/SpeakersInfo.scala
@@ -9,8 +9,11 @@ object SpeakersInfo:
   val jimNewton = Speaker(
     name = "Jim Newton",
     photo = Some("jim.webp"),
-    job = "",
-    company = "EPITA Rennes",
+    description = """|Jim Newton is a junior researcher professor at EPITA (Paris), working in the automata research group at EPITA Research Lab.  He holds a PhD in Computer Science from Sorbonne Université.
+                     |Jim has 35+ years of experience in professional Lisp programming in the IC/VLSI design sector, at Cypress Semiconductor, Intel, and Cadence Design Systems.
+                     |Jim has been programming off and on in Scala for about 5 years.""",
+    job = "Researcher",
+    company = "EPITA Le Kremlin-Bicêtre",
     socials = List(
       Social(Social.Kind.Linkedin, "https://www.linkedin.com/in/jim-newton-463600a8"),
       Social(Social.Kind.Other, "https://www.lrde.epita.fr/wiki/User:Jnewton")
