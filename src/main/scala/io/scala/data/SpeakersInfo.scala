40d36c16d6d7e0b9e55b6c89346d948400d4e81f
Jim Newton <jnewton@lrde.epita.fr>
2024-02-14 13:49:13 +0100

    fixed missing stripMargin

--- src/main/scala/io/scala/data/SpeakersInfo.scala
+++ src/main/scala/io/scala/data/SpeakersInfo.scala
@@ -11,7 +11,8 @@ object SpeakersInfo:
     photo = Some("jim.webp"),
     description = """|Jim Newton is a junior researcher professor at EPITA (Paris), working in the automata research group at EPITA Research Lab.  He holds a PhD in Computer Science from Sorbonne Université.
                      |Jim has 35+ years of experience in professional Lisp programming in the IC/VLSI design sector, at Cypress Semiconductor, Intel, and Cadence Design Systems.
-                     |Jim has been programming off and on in Scala for about 5 years.""",
+                     |Jim has been programming off and on in Scala for about 5 years.
+                     |""".stripMargin,
     job = "Researcher",
     company = "EPITA Le Kremlin-Bicêtre",
     socials = List(
