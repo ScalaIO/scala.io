5db88c06422863a854487951f216fe38a1052976
Lucas Nouguier <lucas.nouguier@protonmail.com>
2024-03-09 14:08:34 +0100

    added first questions

--- src/main/scala/io/scala/views/FAQView.scala
+++ src/main/scala/io/scala/views/FAQView.scala
@@ -1,6 +1,5 @@
 package io.scala.views
 
-import io.scala.domaines.Social
 import io.scala.data.OrgaInfo.allOrga
 import io.scala.domaines.Organizer
 import io.scala.domaines.Social
@@ -8,6 +7,8 @@ import io.scala.modules.elements.Title
 import io.scala.svgs.Github
 
 import com.raquo.laminar.api.L.*
+import org.scalajs.dom.MouseEvent
+import org.scalajs.dom.html
 
 object FAQView extends SimpleView {
   private def question(question: String, answer: String): HtmlElement = {
@@ -30,94 +31,11 @@ object FAQView extends SimpleView {
     sectionTag(
       className := "container faq",
       Title("Frequently Asked Questions"),
-      question(
-        "Why 2023-10 was cancelled?",
-        "It was difficult to get sponsorship to close the budget, that's why we had to work on a new format for this edition."
-      ),
-      question(
-        "Why Nantes?",
-        """The conference was in Paris (2013, 2014, 2022) and Lyon (2016, 2017, 2018, 2019).
-          |The organisers are in majority familiar with Nantes, Clever Cloud and _icilundi offered to facilitate hosting the event.""".stripMargin
-      ),
-      question(
-        "Is there a Code of Conduct?",
-        """We had a Code of Conduct, the new one is lighter : 'please be nice', 'we are here to exchange idea and appreciate a shared moment', 'if there is something disturbing, we will take care of it'.
-          |
-          |We are not avoiding the idea of a CoC and it was discussed with others in the community. Writing something is not the same thing are making sure everyone is welcomed.
-          |So we moved to a more convergent approach,
-          | in continuity with the quality of our event.
-          |""".stripMargin
-      ),
-      question(
-        "Do you reimburse tickets?",
-        "We would prefer not to, however you can always send us an email. We always reimbursed tickets if needed."
-      ),
-      question(
-        "Can I have a receipt?",
-        "In the confirmation email, there is a link to download it. If you can't find it, send us an email and we will send you a new one."
-      ),
-      question(
-        "What did you use to build this website?",
-        ul(
-          li(
-            "Source code: ",
-            a(href := "https://scala-lang.org", "Scala 3"),
-            " with ",
-            a(href := "https://www.scala-js.org", "Scala.js"),
-            " to compile Scala to JavaScript and ",
-            a(href := "https://laminar.dev", "Laminar"),
-            " to build a reactive UI."
-          ),
-          li(
-            "Hosting: ",
-            a(href := "https://github.com/ScalaIO/scala.io", "Github repository"),
-            " for the source code and ",
-            a(href := "https://www.clever-cloud.com", "Clever Cloud"),
-            " for the website"
-          )
-        )
-      ),
-      div(
-        className := "logos",
-        a(
-          href     := "https://www.scala-js.org",
-          nameAttr := "Scala.js",
-          img(
-            src := "logos/scalajs.svg",
-            alt := "Scala.js logo"
-          )
-        ),
-        a(
-          href     := "https://laminar.dev",
-          nameAttr := "Laminar",
-          img(
-            src := "logos/laminar.webp",
-            alt := "Laminar logo"
-          )
-        ),
-        a(
-          href     := "https://www.clever-cloud.com",
-          nameAttr := "Clever Cloud",
-          img(
-            src := "logos/clever.svg",
-            alt := "Clever Cloud logo"
-          )
-        ),
-        a(
-          href     := "https://github.com/ScalaIO/scala.io",
-          nameAttr := "Github",
-          Github()
-        )
-      ),
-      question(
-        "I found a bug / an error on the website, what should I do?",
-        p(
-          "Feel free to open an issue us on the ",
-          a(href := "https://github.com/ScalaIO/scala.io/issues/new", "Github repository"),
-          " or send us an email at ",
-          a(href := "mailto:contact@scala.io", "contact@scala.io")
-        )
-      ),
+      div(),
+      Title.small("Common questions"),
+      commonQuestions,
+      Title.small("Speaker questions"),
+      speakerQuestions,
       Title("Who is behind Scala.IO?"),
       div(),
       Title.small("Representatives"),
@@ -126,6 +44,134 @@ object FAQView extends SimpleView {
       organizersList(allOrga.filterNot(_.representative))
     )
   }
+
+  lazy val commonQuestions = List(
+    question(
+      "Why 2023-10 was cancelled?",
+      "It was difficult to get sponsorship to close the budget, that's why we had to work on a new format for this edition."
+    ),
+    question(
+      "Why Nantes?",
+      """The conference was in Paris (2013, 2014, 2022) and Lyon (2016, 2017, 2018, 2019).
+          |The organisers are in majority familiar with Nantes, Clever Cloud and _icilundi offered to facilitate hosting the event.""".stripMargin
+    ),
+    question(
+      "Is there a Code of Conduct?",
+      """We had a Code of Conduct, the new one is lighter : 'please be nice', 'we are here to exchange idea and appreciate a shared moment', 'if there is something disturbing, we will take care of it'.
+          |
+          |We are not avoiding the idea of a CoC and it was discussed with others in the community. Writing something is not the same thing are making sure everyone is welcomed.
+          |So we moved to a more convergent approach,
+          | in continuity with the quality of our event.
+          |""".stripMargin
+    ),
+    question(
+      "Do you reimburse tickets?",
+      "We would prefer not to, however you can always send us an email. We always reimbursed tickets if needed."
+    ),
+    question(
+      "Can I have a receipt?",
+      "In the confirmation email, there is a link to download it. If you can't find it, send us an email and we will send you a new one."
+    ),
+    question(
+      "What did you use to build this website?",
+      ul(
+        li(
+          "Source code: ",
+          a(href := "https://scala-lang.org", "Scala 3"),
+          " with ",
+          a(href := "https://www.scala-js.org", "Scala.js"),
+          " to compile Scala to JavaScript and ",
+          a(href := "https://laminar.dev", "Laminar"),
+          " to build a reactive UI."
+        ),
+        li(
+          "Hosting: ",
+          a(href := "https://github.com/ScalaIO/scala.io", "Github repository"),
+          " for the source code and ",
+          a(href := "https://www.clever-cloud.com", "Clever Cloud"),
+          " for the website"
+        )
+      )
+    ),
+    div(
+      className := "logos",
+      a(
+        href     := "https://www.scala-js.org",
+        nameAttr := "Scala.js",
+        img(
+          src := "logos/scalajs.svg",
+          alt := "Scala.js logo"
+        )
+      ),
+      a(
+        href     := "https://laminar.dev",
+        nameAttr := "Laminar",
+        img(
+          src := "logos/laminar.webp",
+          alt := "Laminar logo"
+        )
+      ),
+      a(
+        href     := "https://www.clever-cloud.com",
+        nameAttr := "Clever Cloud",
+        img(
+          src := "logos/clever.svg",
+          alt := "Clever Cloud logo"
+        )
+      ),
+      a(
+        href     := "https://github.com/ScalaIO/scala.io",
+        nameAttr := "Github",
+        Github()
+      )
+    ),
+    question(
+      "I found a bug / an error on the website, what should I do?",
+      p(
+        "Feel free to open an issue us on the ",
+        a(href := "https://github.com/ScalaIO/scala.io/issues/new", "Github repository"),
+        " or send us an email at ",
+        a(href := "mailto:contact@scala.io", "contact@scala.io")
+      )
+    )
+  )
+
+  lazy val speakerQuestions = List(
+    question(
+      "How are the talks selected?",
+      p(
+        "We have a call for papers (CFP) whose link is available on the website. The talks are selected by the organizers plus a few external people."
+      ),
+      p(
+        "The selection process is based on the content of the talk and the appeal of the reviewer to the subject. Every reviewer emits a note on each talk and the talks are selected based on the average note. The grade is note the only criterion, we also take into account the diversity of the subjects"
+      )
+    ),
+    question(
+      "How will I be notified if my talk is selected?",
+      p(
+        "We will send you an email to the address you used to submit your talk. But please keep in mind that if you don't answer to our emails, within 2 weeks, we will consider that you declined the invitation. We will then contact the next speaker in the list."
+      )
+    ),
+    question(
+      "Will there be replays?",
+      p(
+        "Yes, we will record the talks and make them available on our Youtube channel (",
+        a(href := "https://www.youtube.com/@scalaio", "https://www.youtube.com/@scalaio"),
+        ") after processing them (checking the sound, adding an intro frame at the start if the video, etc.)"
+      ),
+      p("We references the videos and slides on the website on your talk's page. So please, remember to send us the link/pdf :)")
+    ),
+    question(
+      "Do you reimburse travel expenses?",
+      p(
+        "We will reimburse travel expenses as far as the budget allows. We will send you a form to fill in with your expense expectancies and we will do our best to cover them."
+      )
+    ),
+    question(
+      "Do I have a free ticket?",
+      "Of course :)"
+    )
+  )
 
   def organizersList(orgs: List[Organizer]): Div = div(
     className := "card-container organizers",
