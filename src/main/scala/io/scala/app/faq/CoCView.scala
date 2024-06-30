0dcb05b7ace0fc23b698fc3a2e95c209b3d399af
Jonathan Winandy <jonathan.winandy@gmail.com>
2024-06-30 16:16:48 +0200

    fix Coc: more consensual

--- src/main/scala/io/scala/app/faq/CoCView.scala
+++ src/main/scala/io/scala/app/faq/CoCView.scala
@@ -12,7 +12,7 @@ import io.scala.views.SimpleView
 object CoCView extends SimpleView:
   override def body(): HtmlElement = sectionTag(
     className := "container coc",
-    Titles.withSub("Scala-FR Code of Conduct (SFCoC)", p("Version 0.0.1")),
+    Titles.withSub("Scala-FR Code of Conduct (CoC)", p("Version 0.0.1")),
     Paragraphs.description(
       "This Code of Conduct is heavily inspired form ",
       Links.highlighted(
@@ -23,68 +23,45 @@ object CoCView extends SimpleView:
     ),
     Titles.medium("Introduction"),
     Paragraphs.description(
-      "The Scala-FR Code of Conduct (SFCoC) dictates the terms and conditions underwhich you can freely participate in the community."
+      "The Scala-FR Code of Conduct (CoC) explain the conditions to participate in the community."
     ),
     Paragraphs.description(
-      "The purpose of SFCoC is to facilitate an inclusive and diverse community of professionals who productively work toward shared professional goals."
+      "The purpose of CoC is to facilitate an inclusive and diverse community of professionals who productively work toward shared interests and professional goals."
     ),
     Paragraphs.description(
-      "In service of this purpose, we open the community to all qualified individuals and we require that all members refrain from unprofessional conduct. Except as otherwise noted, we do not impose any ideology onto members, nor do we restrict participation based on people's personal attributes or their conduct in other communities."
+      "In service of this purpose, we open the community to all qualified individuals and we recommend that all members refrain from any unprofessional or uncomfortable conducts."
+    ),
+    Paragraphs.description(
+      "To sum it up: \"please be nice, we are here for a beneficial share moment, else (explained in the reporting section)\""
     ),
     Titles.small("Scope"),
     Paragraphs.description(
-      "SFCoC applies to all members of the community at all times in any French community space, including",
+      "CoC applies in any French community space, including",
       Lists.flat(
-        li(Titles.paragraph("Physical spaces"), "such as meetups, conferences, and workplaces"),
+        li(Titles.paragraph("Physical spaces"), "such as meetups, conferences"),
         li(Titles.paragraph("Virtual spaces"), "such as the Scala-FR discord server")
       )
     ),
     Titles.medium("Community Conduct", idAttr := "community-conduct"),
     Paragraphs.description(
-      "We strongly encourage members to assume the best in every interaction; to be open, honest, and empathic in all communication; and to demonstrate politeness and professional courtesy in every situation."
-    ),
-    Paragraphs.description(
-      "We welcome any closed group of members to engage in any conduct for which there is explicit and unanimous mutual consent. In the absence of such consent, we impose the following restrictions on conduct (which aren't standards for everyone, unfortunately):"
-    ),
-    Lists.flat(
-      li(
-        Titles.paragraph("No harassment"),
-        "Don't force yourself on other people. They must consent to the interaction"
-      ),
-      li(Titles.paragraph("No prying"), "People private content/communications are well... private"),
-      li(
-        Titles.paragraph("No insults"),
-        "Don't be rude, it just makes things worse for everybody (not just the people involved)"
-      ),
-      li(
-        Titles.paragraph("No doxing"),
-        "Do not publish details of members that you learn about. It is a criminal act in France"
-      ),
-      li(
-        Titles.paragraph("No retaliation"),
-        "Do not retaliate against members for the resolution or the reporting of a violation of the SFCoC"
-      )
-    ),
-    Paragraphs.description(
-      "These requirements on conduct apply to members at all times, physically as well as virtually (Twitter/X, LinkedIn, Reddit...), even when they are not actively participating in the community."
+      "We strongly encourage members to assume the best in every interaction, to be open, honest, and empathetic in all communications, and to demonstrate courtesy in every situation. Above all, we must respect each individual's autonomy and free will."
     ),
     Titles.medium("Reporting"),
+    Paragraphs.description(
+      "The reporting and resolution section may be the most important part of this Code of Conduct. Scala-Fr organizers commit to taking all reports seriously and will do everything possible to resolve situations in the best way for all parties involved."
+    ),
     Titles.small("Resolution"),
     Paragraphs.description(
       Titles.paragraph("Unofficial Resolution"),
-      "We encourage as much as possible this resolution mode where aggrieved members speak to accused members directly, using the language of non-violent communication (NVC). At the request of an aggrieved member, we will appoint a mediator who can facilitate an unofficial resolution and serve as a witness for subsequent reporting."
+      "We strongly encourage aggrieved members to speak directly with the accused members using non-violent communication (NVC). Upon request, we will find or appoint a third person to facilitate an unofficial resolution and help mediate the situation."
     ),
     Paragraphs.description(
       Titles.paragraph("Official Resolution"),
-      "For official resolution, aggrieved members must report the incident to us and request an official resolution. We will appoint an arbiter, who will speak individually to all parties, including witnesses, before deciding on a course of action. The course of action will involve rejecting the incident, or accepting it as a violation and imposing a consequence on the violator."
+      "For an official resolution, aggrieved members must report the incident and request an official intervention. We will designate an arbiter, either from within or outside our organization, who will independently speak with all parties involved, including potential third parties knowledgeable about the situation. The arbiter will then decide on a course of action: either rejecting the incident, acknowledging the situation but determining that our organization cannot move forward on it, or indicating it as a violation and recommending appropriate consequences."
     ),
     Paragraphs.description(
       Titles.paragraph("Consequences"),
-      "Violators may be asked to apologize or to undergo training, counseling, or mediation as a condition of continued participation. They may also be banned from the community, at the sole discretion of the arbiter. In no case will the consequence exceed banishment, unless the violation is also governed by separate contractual agreements or local laws. We reserve the right to re-evaluate a previously imposed consequence."
-    ),
-    Paragraphs.description(
-      Titles.paragraph("Confidentiality"),
-      "We reserve the right to publicly share information about any violation that results in banishment, including personally identifiable information on the violator, limited to name and online identities. In all other cases, we will keep incidents and the identities of aggrieved and accused confidential to the extent this confidentiality is fully respected by all parties."
+      "Violators may be asked to apologize, take training, ... as a condition of continued participation. They may also be banned from our community. We may re-evaluate later a previously imposed consequence."
     ),
     Titles.small("Our pledge"),
     p("We pledge that we will make reasonable efforts on"),
@@ -92,67 +69,27 @@ object CoCView extends SimpleView:
       li(
         Paragraphs.description(
           Titles.paragraph("Enforcement"),
-          "Enforcing the terms and conditions set forth in SFCoC, and to hold ourselves to these same standards, thereby setting a positive example for others to follow."
+          "Enforcing the terms and conditions set forth in CoC."
         )
       ),
       li(
         Paragraphs.description(
           Titles.paragraph("Contact Information"),
-          "Providing contact information for use in reporting incidents, including an estimate of how quickly members can expect to receive a response."
-        )
-      ),
-      li(
-        Paragraphs.description(
-          Titles.paragraph("Community Boundaries"),
-          "Providing information on the boundaries of the community, including information on any regions in time or space that are partially or fully exempt from SFCoC."
-        )
-      ),
-      li(
-        Paragraphs.description(
-          Titles.paragraph("Non-Coercion"),
-          "Never coerce members into revealing private information, making statements, or otherwise requiring actions that are unnecessary for the scope of participating in a given community activity."
-        )
-      ),
-      li(
-        Paragraphs.description(
-          Titles.paragraph("Disclosure"),
-          "Upon request from an unqualified individual, we pledge to make reasonable efforts to provide a written, anonymized summary to the individual that discloses the rationale and evidence resulting in their status, and we grant them permission to share this disclosure privately or publicly, providing they do so in its entirety."
-        )
-      ),
-      li(
-        Paragraphs.description(
-          Titles.paragraph("Unbiased Arbitration"),
-          "Appointing balanced or neutral arbiters for every arbitration, without special relationships to aggrieved or accused, or vested interest in any specific outcome. In the event that unbiased arbitration is impossible or too onerous, we pledge to disclose all known biases."
-        )
-      ),
-      li(
-        Paragraphs.description(
-          Titles.paragraph("Merit Transparency"),
-          "Continuously communicate whether the status, authority, or responsibility of individual members is awarded or withheld based on personal attributes that are not relevant to their function in the community."
-        )
-      ),
-      li(
-        Paragraphs.description(
-          Titles.paragraph("Amendments"),
-          "Any part of SFCoC, including but not limited to definitions in the glossary, may be unilaterally amended by us. We pledge to clearly specify and distribute such amendments together with SFCoC."
+          "Providing contact information for use in reporting incidents, including an informations on our internal processes"
         )
       )
     ),
     Titles.small("Disclaimers"),
     Paragraphs.description(
-      Titles.paragraph("Disputes of Interpretation"),
-      "We alone can clarify ambiguities in the SFCoC. We pledge to be consistent in our interpretation of any ambiguity and update the SFCoC to remove ambiguities as they are found."
-    ),
-    Paragraphs.description(
       Titles.paragraph("Criminal Conduct"),
-      "SFCoC is not intended to supplant the laws of governing jurisdictions, but to impose a set of terms and conditions on community membership for law-abiding individuals. Criminal conduct of any kind should be immediately reported to relevant authorities and dealt with as recommended by such authorities. Notwithstanding the foregoing, if criminal conduct also violates SFCoC, we reserve the right to deal with such conduct as described herein."
+      "CoC is not intended to supplant the laws of governing jurisdictions. Criminal conduct of any kind should be immediately reported to relevant authorities and dealt with as recommended by such authorities."
     ),
     Paragraphs.description(
       Titles.paragraph("Non-Endorsement"),
       "In recognition of the pluralistic composition of the community, the community does not endorse the attitudes, beliefs, moral systems, personality traits, political views, preferences, religions, or values of ourselves or of any member of the community. No member ever speaks for or represents the community, and neither do we speak for or represent the community unless we are acting in our official capacities, and then only for the scope of the task at hand."
     ),
     Paragraphs.description(
-      Titles.paragraph("Limitation of Remedy"),
-      "Unless otherwise stated, SFCoC is not intended as a legally binding agreement. In the event we fail to uphold any of our pledges as made herein, remedy is limited to a formal public apology and reversal of any consequences (if applicable), unless the breach is governed by separate contractual agreements or local laws."
+      Titles.paragraph("Limitation"),
+      "CoC is not intended as a legally binding agreement."
     )
   )
