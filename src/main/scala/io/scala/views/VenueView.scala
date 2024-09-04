4b526f0caa71158471a3178ebd10e3d7661199a5
Jonathan Winandy <jonathan.winandy@gmail.com>
2024-09-04 13:40:41 +0200

    add venue

--- src/main/scala/io/scala/views/VenueView.scala
+++ src/main/scala/io/scala/views/VenueView.scala
@@ -9,6 +9,21 @@ object VenueView extends SimpleView {
   def body(): HtmlElement = sectionTag(
     className := "container venue",
     Titles("Our Venue"),
-    div("TODO :)")
+    div("For this edition, we are invited by EPITA, on their site 'Kremlin-Bicêtre'"),
+    img(
+      src := "public/images/places/epita-kb.jpeg"
+    ),
+    br(),
+    Titles("Access"),
+
+    div(
+      "Campus Paris Kremlin-Bicêtre", br(),
+      "Building 'Voltaire', ",
+      a(href := "https://epimap.fr/kb/voltaire/4",
+        "4th floor (epimap)"), br(),
+      a(href := "https://www.google.com/maps/place//data=!4m2!3m1!1s0x47e6718002cb5611:0x581b7c8cd0a77c3e?sa=X&ved=1t:8290&ictx=111",
+        "14-16 rue Voltaire", br(),
+        "FR, 94270 Le Kremlin-Bicêtre"))
+
   )
 }
